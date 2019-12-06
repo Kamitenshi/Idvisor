88c600c9c5140604fe5ceb9f4eb8cf78e32b2de4
Kamitenshi <mikael.simarik@epita.fr>
2019-12-06 14:03:50 +0100

    Home: create title card

--- lrdf-idvisor-front/src/pages/Home.tsx
+++ lrdf-idvisor-front/src/pages/Home.tsx
@@ -1,65 +1,23 @@
+import { IonCard, IonCardTitle, IonImg, IonGrid } from '@ionic/react';
 import React from 'react';
+import Page from '../components/Page';
 import './Home.scss';
-import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonItem, IonIcon, IonLabel, IonButton, IonCardContent } from '@ionic/react';
 
 
 interface HomeProps { };
 
+const titleImagePath = "assets/img/title.jpeg";
+
 const Home: React.FC<HomeProps> = () => {
     return (
-        <IonPage id="home-page">
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonMenuButton />
-                    </IonButtons>
-                    <IonTitle>Home</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardTitle>
-                            Découvrir les métiers du numérique
+        <Page title="home">
+            <IonCard>
+                <img src={titleImagePath} />
+                <IonCardTitle class="card-title">
+                    Découvrir les métiers du numérique
                         </IonCardTitle>
-                    </IonCardHeader>
-                </IonCard>
-                <IonCard>
-                    <IonItem>
-                        <IonIcon name="pin" slot="start" />
-                        <IonLabel>ion-item in a card, icon left, button right</IonLabel>
-                        <IonButton fill="outline" slot="end">View</IonButton>
-                    </IonItem>
-
-                    <IonCardContent>
-                        This is content, without any paragraph or header tags,
-                        within an ion-cardContent element.
-      </IonCardContent>
-                </IonCard>
-
-                <IonCard>
-                    <IonItem href="#" class="activated">
-                        <IonIcon name="wifi" slot="start" />
-                        <IonLabel>Card Link Item 1 .activated</IonLabel>
-                    </IonItem>
-
-                    <IonItem href="#">
-                        <IonIcon name="wine" slot="start" />
-                        <IonLabel>Card Link Item 2</IonLabel>
-                    </IonItem>
-
-                    <IonItem class="activated">
-                        <IonIcon name="warning" slot="start" />
-                        <IonLabel>Card Button Item 1 .activated</IonLabel>
-                    </IonItem>
-
-                    <IonItem>
-                        <IonIcon name="walk" slot="start" />
-                        <IonLabel>Card Button Item 2</IonLabel>
-                    </IonItem>
-                </IonCard>
-            </IonContent>
-        </IonPage>
+            </IonCard>
+        </Page>
     );
 }
 
