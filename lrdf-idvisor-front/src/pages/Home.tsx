e0d070a09a5cf91f246f3b6323cde9c46d33b134
Mikaël Simarik <mikael.simarik@epita.fr>
2019-12-29 21:28:36 +0100

    Home: create basic page

--- lrdf-idvisor-front/src/pages/Home.tsx
+++ lrdf-idvisor-front/src/pages/Home.tsx
@@ -1,14 +1,44 @@
+import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { appName } from '../constants/name';
 import './Home.css';
+
+interface HeaderInterface { }
+
+const Header: React.FC<HeaderInterface> = () => {
+  return (<IonHeader>
+    <IonToolbar>
+      <IonButtons slot='start'>
+        <IonTitle>{appName}</IonTitle>
+      </IonButtons>
+      <IonButtons slot='end'>
+        <IonButton id='button-signup'>S'inscrire</IonButton>
+        <IonButton>Se connecter</IonButton>
+      </IonButtons>
+    </IonToolbar>
+  </IonHeader>)
+}
+
 
 interface HomeInterface {
 }
 
-
-
 const HomePage: React.FC<HomeInterface> = () => {
 
   return (
+    <IonPage>
+      <Header />
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol id='title-cell'>
+              <IonText color='primary' class='title' id='main-title'>Idvisor</IonText>
+              <IonText color='secondary' class='title' id='subtitle'>Trouver sa formation dans le numérique</IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
   )
 }
 
