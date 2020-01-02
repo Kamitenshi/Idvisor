f3271f5eb47167a7994ebbc9707b5492322a1e64
leo menschenfreund <leo.menschenfreund@epita.fr>
2020-01-02 13:36:20 +0100

    gitignore update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,11 @@
-resource "aws_key_pair" "admin" {
-   key_name   = "admin"
+resource "aws_key_pair" "admin2" {
+   key_name   = "admin2"
    public_key = "${var.sshkeypub}"
  }
  resource "aws_instance" "nlpfubuntu" {
    ami           = "ami-087855b6c8b59a9e4"
    instance_type = "t2.micro"
-   key_name      = "admin"
+   key_name      = "admin2"
    tags = {
        tag1 = "nlpf"
        }
