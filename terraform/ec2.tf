dffff1df305688774ed245fa44a57b918b28af9d
leo menschenfreund <leo.menschenfreund@epita.fr>
2020-01-03 13:53:10 +0100

    terraform: update infrastructure files

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,12 +1,24 @@
-resource "aws_key_pair" "admin2" {
-   key_name   = "admin2"
-   public_key = "${var.sshkeypub}"
- }
+resource "aws_key_pair" "deployer" {
+  key_name = "deployer key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC91oprCtxPNXdoIeYRKrIHpPE9wlZRgx8O08yWTJ6Fv7wcByxgaTDfHYQT5+KeFJuJqlgrwmaFGG5gsY77l0g1Ya+UQM0CbP7Bi8DtS8YRD2WMpPHL3Bjkp6VFvMnWtlWr9d6Ecvoyv0wkWec/Yfb6w/RwNbYBivUXj7aAmZH411cAhcRGWmqTcVotNwYMzYW90zaIe7sK+jLVyAv/n38NaqkelYaos0QU3pdqhnNQOL3m2ymt1D9xL5dFHC6NxoKkvJaU8h8DgbMSDOT8oIxoIc3VFG/E8l7CuUHrDp67j5A9AWAHPxQvXZ/3nSZD6SRZksEoRQ8z+mRHzJFqQrIb jigsaw@jigsaw-VirtualBox"
+}
+
  resource "aws_instance" "nlpfubuntu" {
    ami           = "ami-087855b6c8b59a9e4"
    instance_type = "t2.micro"
-   key_name      = "admin2"
+   key_name      = "${aws_key_pair.deployer.key_name}"
    tags = {
        tag1 = "nlpf"
        }
- }
+
+  provisioner "local-exec" {
+    command =  "sleep 10; sudo echo ${aws_instance.nlpfubuntu.public_ip} > /etc/ansible/hosts"
+  }
+  
+  provisioner "local-exec" {
+        command = "sleep 10; export ANSIBLE_HOST_KEY_CHECKING=False; sudo ansible-playbook -u ubuntu --private-key ${var.private_key} playbook-install-docker.yml; pip install docker-compose; sudo ansible-playbook -u ubuntu --private-key ${var.private_key} playbook-docker-compose.yml"
+    }
+
+
+}
+
--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -9,6 +9,12 @@ resource "aws_default_security_group" "default" {
      # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
      cidr_blocks     = ["0.0.0.0/0"]
    }
+ ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
  egress {
      from_port       = 0
      to_port         = 0
