50d9dd11a7b466159a8548832332f74bcf9a1d1b
Ubuntu <ubuntu@ip-172-31-25-95.eu-west-3.compute.internal>
2020-01-05 14:44:32 +0000

    terraform: wait until ec2 deploy and change .env path

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -16,7 +16,7 @@ resource "aws_key_pair" "deployer" {
   }
   
   provisioner "local-exec" {
-        command = "sleep 10; export ANSIBLE_HOST_KEY_CHECKING=False; sudo ansible-playbook -u ubuntu --private-key ${var.private_key} playbook-install-docker.yml; pip install docker-compose; sudo ansible-playbook -u ubuntu --private-key ${var.private_key} playbook-docker-compose.yml"
+        command = "sleep 120; export ANSIBLE_HOST_KEY_CHECKING=False; sudo ansible-playbook -u ubuntu --private-key ${var.private_key} playbook-install-docker.yml; pip install docker-compose; sudo ansible-playbook -u ubuntu --private-key ${var.private_key} playbook-docker-compose.yml"
     }
 
 
