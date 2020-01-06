ff913dbc0074a717ca27aa77add51fce1e78bb74
Mikaël Simarik <mikael.simarik@epita.fr>
2020-01-06 08:45:29 +0100

    CORS: fix allow headers

--- lrdf-idvisor-back/src/app.ts
+++ lrdf-idvisor-back/src/app.ts
@@ -36,7 +36,7 @@ class App {
         this.app.use(morgan("dev"));
         this.app.use(cors({
             credentials: true,
-            origin: env.FRONT_ADDRESS //TODO: set in environment variable
+            origin: 'http://' + env.FRONT_ADRESS //TODO: set in environment variable
         }));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
