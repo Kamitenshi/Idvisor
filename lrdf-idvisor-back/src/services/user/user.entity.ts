017f8080566471a8272f78a0ea9583720f9143ec
Mikaël Simarik <mikael.simarik@epita.fr>
2020-01-07 10:38:07 +0100

    Database: create tables student, advisor, workshop, skill, field

--- lrdf-idvisor-back/src/services/user/user.entity.ts
+++ lrdf-idvisor-back/src/services/user/user.entity.ts
@@ -1,6 +1,7 @@
 import { IsEmail, IsString } from 'class-validator'
-import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm"
 import { Conversation, Message } from '../chat/chat.entity'
+import { Workshop } from '../workshop/workshop.entity'
 
 export type Role = "admin" | "student" | "advisor"
 
@@ -21,11 +22,64 @@ class UserDB {
   @Column()
   public password!: string
 
-  @OneToMany(type => Message, message => message.author)
+  @OneToMany(type => Message, message => message.author, { onDelete: 'CASCADE' })
   public messages!: Message[]
 
-  @ManyToMany(type => Conversation, conv => conv.users)
+  @ManyToMany(type => Conversation, conv => conv.users, { onDelete: 'CASCADE' })
   public conversations!: Conversation[]
+}
+
+@Entity()
+export class Skill {
+  @OneToOne(_ => Field, { primary: true })
+  @JoinColumn()
+  public field!: Field
+  @Column()
+  public level!: number
+  @ManyToMany(_ => Student, student => student.user)
+  public students!: Student[]
+  @ManyToMany(_ => Workshop, w => w.id)
+  public workshops!: Workshop[]
+}
+
+@Entity()
+export class Field {
+  @PrimaryGeneratedColumn()
+  public id!: number
+  @Column()
+  public name!: string
+  @ManyToMany(_ => Student, student => student.user)
+  public students!: Student[]
+}
+
+@Entity()
+export class Student {
+  @OneToOne(type => UserDB, { primary: true })
+  @JoinColumn()
+  public user!: UserDB
+
+  @ManyToMany(_ => Skill, skill => skill.field)
+  @JoinTable()
+  public skills!: Skill[]
+
+  @ManyToMany(_ => Field, cof => cof.id)
+  @JoinTable()
+  public centerOfInterests!: Field[]
+
+  @ManyToMany(_ => Workshop, workshop => workshop.id)
+  @JoinTable()
+  public workshops!: Workshop[]
+}
+
+@Entity()
+export class Advisor {
+  @OneToOne(type => UserDB, { primary: true })
+  @JoinColumn()
+  public user!: UserDB
+  @ManyToMany(_ => Workshop, workshop => workshop.id)
+  @JoinColumn()
+  public workshops!: Workshop[]
+
 }
 
 export class RegisteringUser {
---
+++ lrdf-idvisor-back/src/services/workshop/workshop.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, JoinColumn, ManyToMany, PrimaryGeneratedColumn } from "typeorm"
+import { Advisor, Skill, Student } from "../user/user.entity"
+
+@Entity()
+export class Workshop {
+    @PrimaryGeneratedColumn()
+    public id!: number
+    @Column()
+    public name!: string
+    @ManyToMany(_ => Advisor, advisor => advisor.user)
+    public advisors!: Advisor[]
+    @ManyToMany(_ => Student, student => student.user)
+    public students!: Student[]
+    @ManyToMany(_ => Skill, skill => skill.field)
+    @JoinColumn()
+    public skills!: Skill[]
+}
